a3fb74be84a1f0e6524a75e6cca4d8cd98af9962
zhongpei <1000@intry.cn>
2024-06-12 09:36:25 +0800

    srcDir:'src/'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
+import { resolve } from 'path';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // appDir: 'src',
+  srcDir: 'src',
+  appDir: 'src',
   devtools: { enabled: true },
   modules: ['@nuxt/content'],
+  content: {
+    sources: {
+      content: {
+        driver: 'fs',
+        prefix: '/docs', // All contents inside this source will be prefixed with `/notes`
+        base: resolve(__dirname, './content'),
+      },
+    },
+  },
   routeRules: {
     '/': { prerender: true },
     '/sub/**': { prerender: true },
