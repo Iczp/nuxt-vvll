1abb4c57852b41171ddfcfc52edd1c33ede015b6
zhongpei <1000@intry.cn>
2024-06-25 14:29:48 +0800

    project-configuration

---
+++ src/components/content/ProseA.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const target = ref('');
+const attrs = useAttrs();
+const href = attrs.href as string | undefined;
+if (href?.startsWith('http')) {
+  target.value = '_blank';
+}
+</script>
+
+<template>
+  <a :target="target" v-bind="$attrs">
+    <!-- {{ $slots }} -->
+    <!-- {{ $attrs }} -->
+    <slot></slot>
+  </a>
+</template>
