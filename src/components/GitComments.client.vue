b58bce98ad66d843cab08abc52902aeb9f10e841
zhongpei <1000@intry.cn>
2024-06-27 15:38:41 +0800

    ok

--- src/components/GitComments.client.vue
+++ src/components/GitComments.client.vue
@@ -16,15 +16,47 @@ const init = () => {
 onMounted(() => {
   setTimeout(init, 0);
 });
+
+const tabs = ref([
+  {
+    title: '评论',
+    icon: 'bi:github',
+  },
+  {
+    title: '意见',
+    icon: 'vscode-icons:file-type-wxss',
+  },
+]);
 </script>
 
 <template>
-  <div id="gitalk-container" class="leading-normal">
-    <slot>...加载中...</slot>
-  </div>
+  <Tabs :items="tabs" class="mt-4">
+    <template v-slot:item="{ item }">
+      <div class="flex flex-row items-center gap-2 p-2 text-base">
+        <Icon :name="item.icon" class="size-5" />
+        <span>{{ item.title }}</span>
+      </div>
+    </template>
+
+    <template v-slot="{ index }">
+      <div
+        id="gitalk-container"
+        class="leading-normal"
+        :class="{ hidden: index != 0 }"
+      >
+        <slot>...加载中...</slot>
+      </div>
+
+      <div
+        class="flex flex-col items-center justify-center p-4 mt-4 box min-h-32"
+        :class="{ hidden: index != 1 }"
+      >
+        <Icon name="radix-icons:shadow-none" class="text-red-500 size-12" />
+        未开放的功能！
+      </div>
+    </template>
+  </Tabs>
 </template>
 <style scoped>
-.gt-no-init {
-  gap: 4;
-}
+
 </style>
