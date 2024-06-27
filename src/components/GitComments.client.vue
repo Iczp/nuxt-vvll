9e63aa8e1b447fa91c770eae489a33e3a23530e4
zhongpei <1000@intry.cn>
2024-06-27 17:05:17 +0800

    update labels

--- src/components/GitComments.client.vue
+++ src/components/GitComments.client.vue
@@ -7,8 +7,8 @@ const init = () => {
   var gitalk = new Gitalk({
     clientID: 'Ov23li75reFeYTpJhaI2',
     clientSecret: '38dcb5ed87ada5c1bf41b42dfed1053414345655',
-    labels: ['comment'],
-    title: route.path,
+    labels: ['comment', location.pathname],
+    // title: route.path,
     repo: 'nuxt-vvll',
     owner: 'iczp',
     admin: ['iczp'],
